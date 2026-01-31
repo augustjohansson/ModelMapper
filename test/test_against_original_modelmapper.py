28608a6ffaad3c473bd3b79d5b04f2f02de72661
August Johansson <august.johansson@sintef.no>
2026-01-31 16:15:59 +0100

    Add framework for tests

---
+++ test/test_against_original_modelmapper.py
@@ -0,0 +1,37 @@
+import pytest
+
+
+def test_bpx_to_cidemod_conversion():
+    # Check that the conversion made by the updated modelmapper
+    # matches the original original
+    pass
+
+
+def test_bpx_to_battmo_conversion():
+    # Check that the conversion made by the updated modelmapper
+    # matches the original original
+    pass
+
+
+def test_cidemod_to_battmo_conversion():
+    # Check that the conversion made by the updated modelmapper
+    # matches the original original
+    pass
+
+
+def test_cidemod_to_bpx_conversion():
+    # Check that the conversion made by the updated modelmapper
+    # matches the original original
+    pass
+
+
+def test_battmo_to_bpx_conversion():
+    # Check that the conversion made by the updated modelmapper
+    # matches the original original
+    pass
+
+
+def test_battmo_to_cidemod_conversion():
+    # Check that the conversion made by the updated modelmapper
+    # matches the original original
+    pass
---
+++ test/test_validation.py
@@ -0,0 +1,16 @@
+import pytestx
+
+
+def test_validate_bpx():
+    # Check that the generated bpx files run in pybamm
+    pass
+
+
+def test_validate_cidemod():
+    # Check that the generated cidemod files run in cidemod
+    pass
+
+
+def test_validate_battmo():
+    # Check that the generated battmo files run in battmo
+    pass
